a731e8ba62dbde9bbafdff9c584d50bbf118ad54
Eswar <eswarbev05@gmail.com>
2026-06-25 19:13:57 +0530

    add 3737_Count_Subarrays_With_Majority_Element_I

---
+++ 3001-4000/3737_Count_Subarrays_With_Majority_Element_I.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int balance = n+1;
+
+        int [] freq = new int [n * 2 + 2];
+        int [] pref = new int [n * 2 + 2];
+
+        freq[balance] = 1;
+        pref[balance] = 1;
+
+        int answer = 0;
+
+        for(int num : nums){
+            if(num == target){
+                balance++;
+            }else{
+                balance--;
+            }
+
+            freq[balance]++;
+
+            pref[balance] = pref[balance - 1] + freq[balance];
+            answer += pref[balance - 1];
+        }
+        return answer;
+    }
+}
